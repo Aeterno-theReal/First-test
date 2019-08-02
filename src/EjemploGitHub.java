4b4896fe05ee8cb1f9d6a479206a310bcd97f423
PSI <PSI@aula9-1>
2019-08-02 12:52:35 +0200

    Se añade otra línea de texto para probar el commit y push

--- src/EjemploGitHub.java
+++ src/EjemploGitHub.java
@@ -4,6 +4,7 @@ public class EjemploGitHub {
 	public static void main(String[] args) {
 
 		System.out.println("Hola mundo de GitHub");
+		System.out.println("y hacemos un commit and push para probarlo");
 		System.out.println("Adios!!!");
 
 	}
